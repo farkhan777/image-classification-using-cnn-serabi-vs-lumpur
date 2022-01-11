d95f1ad5099e0c83f8f8e99b4f30c30e591c28de
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-01-11 19:40:40 +0700

    rename class dictionary

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
 model = load_model('serabi_lumpur_class_model.h5')
 
-class_dict = {0: 'kue lumpur', 1: 'kue serabi'}
+class_dict = {0: 'Kue Lumpur', 1: 'Kue Serabi'}
 
 def predict_label(img_path):
     query = cv2.imread(img_path)
